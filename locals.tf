ed32e7cc30eeeb8103ce5f589a387356df126cd4
Kiril Soshnin <me@kirilsoshnin.com>
2024-10-06 11:59:25 -0400

    feat: setup github workspace

--- locals.tf
+++ locals.tf
@@ -9,7 +9,13 @@ locals {
       description         = "Example description for the workspace"
       execution_mode      = "remote"
       project_id          = module.project["ks-eci-project"].id
-      vcs_repo_identifier = "KirilSoshnin/ks-eci-terraform-tfe"
+      vcs_repo_identifier = "${var.github_organization_name}/ks-eci-terraform-tfe"
+    }
+    "ks-eci-github" = {
+      description         = "Example automation workspace for GitHub resources"
+      execution_mode      = "local"
+      project_id          = module.project["ks-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/ks-eci-terraform-github"
     }
   }
 }
